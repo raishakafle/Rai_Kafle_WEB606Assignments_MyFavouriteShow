620088a1bccd63fca02d8ddc80c4a16377e72603
raisakafle <113300872+raisakafle@users.noreply.github.com>
2023-07-07 14:48:44 -0400

    update

---
+++ Rai_Kafle_WEB606Assignments_MyFavouriteShow/src/app/type-filter.pipe.ts
@@ -0,0 +1,30 @@
+// import { Pipe, PipeTransform } from '@angular/core';
+
+// @Pipe({
+//   name: 'typeFilter'
+// })
+// export class TypeFilterPipe implements PipeTransform {
+
+//   transform(value: unknown, ...args: unknown[]): unknown {
+//     return null;
+//   }
+
+// }
+
+import { Pipe, PipeTransform } from '@angular/core';
+import { Content } from './helper-files/content-interface';
+
+@Pipe({
+  name: 'typeFilter'
+})
+export class TypeFilterPipe implements PipeTransform {
+  transform(contentList: Content[], type: string): Content[] {
+    if (!type) {
+      // Filter content with no type
+      return contentList.filter(content => !content.type);
+    } else {
+      // Filter content by type
+      return contentList.filter(content => content.type === type);
+    }
+  }
+}
